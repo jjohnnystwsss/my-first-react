49142542923ef050227a1831a739e0e73193f6a0
jjohnnystw <jjohnnystwsss@gmail.com>
2020-06-02 21:32:28 +0800

    Updates

---
+++ precache-manifest.f280b2a662a925a18b097eca7ad2f6d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a9010d87179c9a5a179a9972a8b0f4",
+    "url": "/my-first-react/index.html"
+  },
+  {
+    "revision": "f89a63a04a7dfbf1407d",
+    "url": "/my-first-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "65969750947a715475e0",
+    "url": "/my-first-react/static/js/2.2e06293d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-first-react/static/js/2.2e06293d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f89a63a04a7dfbf1407d",
+    "url": "/my-first-react/static/js/main.0a16b6ec.chunk.js"
+  },
+  {
+    "revision": "dd98bdcc63c4433c4176",
+    "url": "/my-first-react/static/js/runtime-main.52a4880b.js"
+  }
+]);
